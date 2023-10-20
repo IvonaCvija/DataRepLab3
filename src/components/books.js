0bd142cd6aa35c590bac97c019b7c18ce87c9420
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2023-10-20 13:04:58 +0100

    commentsAddedBooks.js

--- src/components/books.js
+++ src/components/books.js
@@ -1,3 +1,4 @@
+//importing the BookItems component from a file called "bookItems"
 import BookItems from "./bookItems";
 
 function Books(props){
@@ -5,7 +6,11 @@ function Books(props){
     return props.myBooks.map(
         (book)=>{
             return <BookItems myBook={book}  key={BookItems.isbn}></BookItems>
+            //show the BookItems component for each book from "myBooks" array
+            //pass the "book" object as a prop to the BookItems component
+            //use the "isbn" property of the BookItems component as the key
         }
     );
 }
+//exporting the Books component
 export default Books;
