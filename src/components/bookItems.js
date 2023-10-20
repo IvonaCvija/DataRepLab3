367861286059222d2781669e655fc817973d83dc
Ivona Cvija <123738999+IvonaCvija@users.noreply.github.com>
2023-10-20 13:02:35 +0100

    Update bookItems.js

--- src/components/bookItems.js
+++ src/components/bookItems.js
@@ -1,3 +1,4 @@
+//importing CardBody and Card from react-bootstrap
 import { CardBody } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
 
@@ -5,21 +6,29 @@ function BookItems(props) {
 
     return (
         <div>
+            {/*creating a Card component*/}
             <Card>
+                {/*displaying the book title in the card header*/}
                 <Card.Header>{props.myBook.title}</Card.Header>
                 <CardBody>
                     <blockquote className="blockquote mb-0">
+                        {/*displaying the book's thumbnail image*/}
                         <img src={props.myBook.thumbnailUrl}></img>
                         <footer>
+                            {/*displaying the first author of the book*/}
                             {props.myBook.authors[0]}
                         </footer>
                     </blockquote>
                 </CardBody>
             </Card>
+            {/*displaying the book title*/}
             <h3>{props.myBook.title}</h3>
+            {/*displaying the book's thumbnail image*/}
             <img src={props.myBook.thumbnailUrl}></img>
+            {/*sisplaying the first author of the book in a paragraph element*/}
             <p>{props.myBook.authors[0]}</p>
         </div>
     );
 }
+//exporting the BookItems component for use in other parts of the application
 export default BookItems;
